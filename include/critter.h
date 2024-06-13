173f1f70b23bcf072a532617b7fbe28f8aa89cab
pwsmigiel <p.w.smigiel@wp.pl>
2024-06-13 14:27:05 +0200

    Update critter.h
    próbowałam dodac grafiki z odczytu z jednej grafiki ale jeszcze sie nie udało inne zrobiłam przykładowo z osobnymi plikami grafik tylko nie załaczyłam sciezek

--- include/critter.h
+++ include/critter.h
@@ -1,7 +1,9 @@
 #ifndef critter_h
 #define critter_h
-
 #include <iostream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include <vector>
 #include "entity.h"
 class entity;
 class critter:public entity
@@ -9,20 +11,27 @@ class critter:public entity
 public:
     bool take_dmg(int dmg);
     void move(int worldWidth, int worldHeight);
-    void setPosition(int x, int y);
-    critter(int x, int y,int hp);
+    critter(int x, int y, int hp, const std::vector<std::string>& textureFiles);
     ~critter();
+      void draw(sf::RenderWindow &window);
+protected:
+      std::vector<sf::Texture> textures;
+      sf::Sprite sprite;
+      size_t currentTextureIndex;
+};
+class harv_crit : public critter {
+public:
+    harv_crit(int x, int y, int hp, const std::vector<std::string>& textureFiles);
+    virtual ~harv_crit();
+
+    bool harvest();
+    int getHarvestCount() const;
+ bool collected_today;
+    void resetDailyState();
 protected:
 
-};
-class harv_crit:public critter
-{
-public:
-    harv_crit(int x, int y,int hp);
-    ~harv_crit();
-    bool collected_today;
-protected:
-    
+    int harvest_count;  // Licznik
+
 };
 class cow:public harv_crit
 {
@@ -34,22 +43,35 @@ public:
 class sheep:public harv_crit
 {
 public:
-    sheep(int x, int y);
-    ~sheep();
+    sheep(int x, int y) ;
+
+  ~sheep();
 };
 class non_harv_crit:public critter
 {
 public:
+    void resetDailyState();
     //spawnable item as item in imtems.h must be changed!!!!
-    non_harv_crit(int x, int y,int hp);
+   non_harv_crit(int x, int y, int hp, const std::vector<std::string>& textureFiles);
     ~non_harv_crit();
+    bool harvest();  // Nowa metoda
+    int getHarvestCount() const;  // Nowa metoda
+protected:
+    bool collected_today;
+    int harvest_count;
 };
-class chicken:public non_harv_crit
-{
-
+class chicken : public non_harv_crit {
 public:
     chicken(int x, int y);
-    ~chicken();
+    virtual ~chicken();
+
+    void loadAnimation(const std::string& file, int frameWidth, int frameHeight);
+    void updateAnimation();
+
+private:
+    std::vector<sf::IntRect> animationFrames;
+    size_t currentFrame;
+    sf::Clock animationClock;
 };
 class pig:public non_harv_crit
 {
@@ -58,6 +80,9 @@ public:
     ~pig();
 };
 
+
+
+#endif
 
 
 #endif
