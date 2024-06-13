8abbbeb36ced4cb5c4cc326ed957eab77fe90ea8
pwsmigiel <p.w.smigiel@wp.pl>
2024-06-13 22:46:10 +0200

    Update plants.cpp

--- src/plants.cpp
+++ src/plants.cpp
@@ -1,26 +1,72 @@
-//plant functions
 #include "plants.h"
-void plant::water()
-{
-    watered=true;
-}
-plant::plant(int x,int y) : entity(x,y,1) {current_stage=freshly_planted;watered=false;}
-plant::~plant() {} 
-pickupable::pickupable(int x,int y):plant(x,y){}
-pickupable::~pickupable(){}
 
-carrot::carrot(int x,int y):pickupable(x,y){}
-carrot::~carrot(){}
+#include <iostream>
 
-potato::potato(int x,int y):pickupable(x,y){}
-potato::~potato(){};
-//cutable functions
-cutable::cutable(int x,int y):plant(x,y){}
-cutable::~cutable(){}
-//wheat functions
-wheat::wheat(/* args */)
-{
+Plant::Plant(int x, int y, const std::string& textureFile)
+    : x(x), y(y), currentDay(0), growthStage(0), readyForHarvest(false) {
+    loadTextures(textureFile);
+    sprite.setTexture(this->textures[0]);
+    sprite.setPosition(static_cast<float>(x), static_cast<float>(y));
 }
-wheat::~wheat()
-{
+
+Plant::~Plant() {}
+
+void Plant::update(int dayCounter) {
+    if (dayCounter - currentDay >= 2) {
+        currentDay = dayCounter;
+        growthStage++;
+        if (growthStage < textures.size()) {
+            sprite.setTexture(textures[growthStage]);
+        }
+        if (growthStage >= 4) {
+            readyForHarvest = true;
+        }
+    }
 }
+
+void Plant::draw(sf::RenderWindow& window) {
+    window.draw(sprite);
+}
+
+bool Plant::isReadyForHarvest() const {
+    return readyForHarvest;
+}
+
+bool Plant::harvest() {
+    if (readyForHarvest) {
+
+        growthStage = 0;
+        readyForHarvest = false;
+        sprite.setTexture(textures[growthStage]);
+        return true;
+    }
+    return false;
+}
+
+void Plant::loadTextures(const std::string& textureFile) {
+    sf::Image image;
+    if (!image.loadFromFile(textureFile)) {
+        std::cerr << "Error loading texture from file: " << textureFile << std::endl;
+        return;
+    }
+
+    int width = 16;
+    int height = 16;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            sf::Texture texture;
+            texture.loadFromImage(image, sf::IntRect(j * width, i * height, width, height));
+            textures.push_back(texture);
+        }
+    }
+}
+
+Carrot::Carrot(int x, int y)
+    : Plant(x, y, "pa2.png") {}
+
+Carrot::~Carrot() {}
+
+Potato::Potato(int x, int y)
+    : Plant(x, y, "pa2.png") {}
+
+Potato::~Potato() {}
