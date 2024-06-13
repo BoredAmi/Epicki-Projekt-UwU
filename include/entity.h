21e4e0c34bed13f1a0749337b6a6dddb602e9d4b
pwsmigiel <p.w.smigiel@wp.pl>
2024-06-13 14:29:27 +0200

    Update entity.h

--- include/entity.h
+++ include/entity.h
@@ -1,5 +1,4 @@
 
-
 #ifndef ENTITY_H
 #define ENTITY_H
 
@@ -10,7 +9,7 @@ public:
     int getY() const;
     int getHp() const;
     void setPosition(int x, int y);
-    entity(int x, int y,int hp);
+     entity(int x, int y, int hp) : x(x), y(y), hp(hp) {}
     virtual ~entity();
 protected:
     int x;
@@ -19,4 +18,3 @@ protected:
 };
 
 #endif // ENTITY_H
-
